06a5a62a29ccc5491cc8c104947b0c21d936c15c
Raghav Srinivaas M <63859534+srinivaas1998@users.noreply.github.com>
2020-07-04 12:28:46 +0530

    Create Honda.java

---
+++ Honda.java
@@ -0,0 +1,30 @@
+// Using Multiple Interfaces
+public interface Honda {
+	public void petrol();
+
+}
+interface Tesla{
+	public void electric();
+}
+class Car implements Honda,Tesla{
+	public void petrol() {
+		System.out.println("Petrol based cars were at top once.");
+	}
+	public void electric() {
+		System.out.println("Nowadays people prefer electric over petrol.");
+	}
+}
+class Automobile{
+	public static void main(String[] args) {
+		Car c=new Car();
+		c.petrol();
+		c.electric();
+	}
+}
+
+
+//Output
+/*
+Petrol based cars were at top once.
+Nowadays people prefer electric over petrol.
+*/
