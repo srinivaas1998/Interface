478d16d2dad52edf1923698b85b7ec472e5a7b90
Raghav Srinivaas M <63859534+srinivaas1998@users.noreply.github.com>
2020-07-04 13:12:31 +0530

    Create Languages.java

---
+++ Languages.java
@@ -0,0 +1,34 @@
+// Interface in Java 8 with default method
+public interface Languages {
+	void html();
+	void css();
+	void javaScript();
+	default void msg() {
+		System.out.println("Notepad can be used as default for witing code."); //Default Method
+	}
+}
+class WebDevelopment implements Languages{  
+	public void html(){
+		System.out.println("HTML is used for developing WebPages");
+	}  
+	public void css(){
+		System.out.println("CSS is used for styling webpages");
+	}
+	public void javaScript(){
+		System.out.println("JavaScript is used for performing operations on the webpage");
+	}
+}  
+class Testing{  
+	public static void main(String args[]){  
+		WebDevelopment w =new WebDevelopment();  
+		w.html();  
+		w.css();
+		w.javaScript();
+}}
+
+//Output
+/*
+HTML is used for developing WebPages
+CSS is used for styling webpages
+JavaScript is used for performing operations on the webpage
+*/
