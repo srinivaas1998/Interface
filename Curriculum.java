68043293ba186422156d6d067b15755ed6d2c97e
Raghav Srinivaas M <63859534+srinivaas1998@users.noreply.github.com>
2020-07-04 12:09:15 +0530

    Create Curriculum.java

---
+++ Curriculum.java
@@ -0,0 +1,20 @@
+public interface Curriculum {
+	
+		public void learn();
+		public void code();
+}
+class Student implements Curriculum{
+	public void learn(){
+		System.out.println("A student must learn code before starting to code");
+	}
+	public void code(){
+		System.out.println("A student must code only after learning ");
+	}  
+}
+class Testing{
+	public static void main(String[] args){
+		Student s=new Student();
+		s.learn();
+    s.code();
+	}
+}
